0b34228ad29155cbe7d4518810912ab109d79a34
denise baker <denviol@yahoo.com>
2020-03-23 16:44:16 -0500

    Add project files.

---
+++ RandomSquare/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var randomNumbers = new List<double>();
+            var squares = new List<double>();
+            for (int i = 0; i < 51; i++)
+            {
+                randomNumbers.Add(random.Next(51));
+                var squared = randomNumbers[i] * randomNumbers[i];
+                Console.WriteLine("squared " + squared);
+                squares.Add(squared);
+
+            }
+
+            var evens = (from n in randomNumbers where (n % 2 == 0) select n);
+            foreach (var n in evens)
+            {
+                Console.WriteLine("Evens " + n);
+            }
+
+          
+        }
+    }
+}
